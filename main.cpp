9aef929cd3b3b9f1bdadbee848cbefc093875c69
kokhnovich <akokhnovich@yandex.ru>
2019-09-25 22:16:17 +0300

    refactoring

--- main.cpp
+++ main.cpp
@@ -1,171 +1,29 @@
 #include <bits/stdc++.h>
+
+// #pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
 using namespace std;
 
 template<typename T>
 using Matrix = vector<vector<T>>;
-
-template<typename T>
-Matrix<T> mult(Matrix<T> A, Matrix<T> B) {
-  if (A.empty() || B.empty() || A[0].size() != B.size()) throw logic_error("wrong sizes");
-  size_t N = A.size(), M = A[0].size(), K = B[0].size();
-  Matrix<T> R(N, vector<T>(K, T(0)));
-  for (size_t i = 0; i < N; i++) {
-    for (size_t j = 0; j < K; j++) {
-      for (size_t k = 0; k < M; k++) {
-        R[i][j] += A[i][k] * B[k][j];
-      }
-    }
-  }
-  return R;
-}
-
-template<typename T>
-Matrix<T> generateRandomSymmetricMatrix(int size_, int min_elem = -100, int max_elem = 100) {
-  std::uniform_int_distribution<std::mt19937_64::result_type> udist(min_elem, max_elem);
-  std::mt19937_64 generator(std::random_device{}());
-  Matrix<T> a(size_, vector<T>(size_));
-  for (int i = 0; i < size_; ++i) {
-    for (int j = 0; j < size_; ++j) {
-      if (j < i) {
-        a[i][j] = a[j][i];
-      } else {
-        a[i][j] = udist(generator);
-      }
-    }
-  }
-  return a;
-}
-
-double EPS = 1e-5;
-
-inline bool compareDouble(double d1, double d2) {
-  return abs(d1 - d2) <= EPS;
-}
-
-inline bool compareVectorOfDouble(const vector<double>& A1, const vector<double>& A2) {
-  assert(A1.size() == A2.size());
-  for (int i = 0; i < A1.size(); ++i) {
-    if (!compareDouble(A1[i], A2[i])) {
-      return false;
-    }
-  }
-  return true;
-}
-
-inline bool compareMatrixOfDouble(const Matrix<double>& A1, const Matrix<double>& A2) {
-  assert(A1.size() == A2.size());
-  for (int i = 0; i < A1.size(); ++i) {
-    if (!compareVectorOfDouble(A1[i], A2[i])) {
-      return false;
-    }
-  }
-  return true;
-}
-
-template<typename T>
-vector<T> generateAnsMatrix(const Matrix<T>& A, const vector<T>& x) {
-  vector<T> ans(A.size());
-  for (int i = 0; i < A.size(); ++i) {
-    ans[i] = T(0);
-    for (int j = 0; j < A.size(); ++j) {
-      ans[i] += x[j] * A[i][j];
-    }
-  }
-  return ans;
-}
-
-/// Nx1 == true  <=> ans.size() == n && ans[i].size() == 1 \forall i
-/// Nx1 == false <=> ans.size() == 1 && ans[i].size() == n \forall i
-template<typename T>
-Matrix<T> make2Dfrom1D(const vector<T>& a, bool Nx1 = true) {
-  if (Nx1) {
-    Matrix<T> ans(a.size(), vector<T>(1));
-    for (int i = 0; i < a.size(); ++i) {
-      ans[i][0] = a[i];
-    }
-    return ans;
-  } else {
-    Matrix<T> ans(1, vector<T>(a.size()));
-    for (int i = 0; i < a.size(); ++i) {
-      ans[0][i] = a[i];
-    }
-    return ans;
-  }
-}
-
-// @TODO add functionality
-template<typename T>
-vector<T> make1Dfrom2D(const Matrix<T>& a) {
-  vector<T> ans(a.size());
-  for(int i = 0; i < a.size(); ++i) {
-    ans[i] = a[i][0];
-  }
-  return ans;
-}
 
 // @TODO LABAAA
 // @TODO norm tests
 // @TODO optimize using profiler
 // @TODO class LatexWriter
 
-template<class T>
-struct greater_using_abs {
-  bool operator()(const T& x, const T& y) const { return abs(x) > abs(y); }
-};
 
-template<typename T>
-Matrix<T> getHWMatrix(int n) {
-  return {{n + 1, n / 2, -n / 2, 1},
-          {-n - 1, -(n + 1) / 2, (n + 1) / 2, -(n + 2) / 3},
-          {-n + 1, (n + 1) / 2, -(n + 2) / 3, n - 1},
-          {n / 3, -1, n, -n}};
-}
-
-template<typename T>
-Matrix<T> getIdentityMatrix(int n) {
-  Matrix<T> A(n, vector<T>(n, T(0)));
-  for (size_t i = 0; i < n; ++i) {
-    A[i][i] = T(1);
-  }
-  return A;
-}
-
+#include "utils.cpp"
 #include "Fraction.h"
 #include "test_runner.h"
 #include "Debug.h"
 #include "Solver.cpp"
 #include "tests.cpp"
 #include "profile.h"
-
-void SolveMyHomeWorkAboutDLUP() {
-  Matrix < Fraction > A = {{1, 3, 2},
-                           {3, 5, 7},
-                           {4, 5, 8}}, L, D, U, P;
-  auto solver = new Solver<Fraction, greater_using_abs<Fraction>>();
-
-  PrintMatrix(A, "A before");
-  tie(D, L, U, P) = solver->DLUP_Decomposition(A);
-  PrintMatrix(A, "A after");
-  PrintMatrix(D, "D");
-  PrintMatrix(L, "L");
-  PrintMatrix(U, "U");
-  PrintMatrix(P, "P");
-
-  auto dlup = mult(solver->GetReversed(D), mult(L, mult(U, solver->GetReversed(P))));
-  PrintMatrix(dlup, "D^{-1}LUP^{-1}");
-
-  dlup = mult(mult(solver->GetReversed(D), L), mult(U, solver->GetReversed(P)));
-  PrintMatrix(dlup, "(D^{-1}L) (UP^{-1})");
-
-  cout << (dlup == A ? "Wonderful!" : "wrong") << endl;
-  assert(dlup == A);
-  PrintMatrix(mult(L, U), "LU");
-  PrintMatrix(mult(mult((D), A), (P)), "DAP");
-
-  PrintMatrix(mult(D, P), "DP");
-}
-
 #include "cma_lab_tasks.cpp"
+
 int main() {
 //  PrintMatrix(generateRandomSymmetricMatrix<Fraction>(10), "random sym matrix");
 //  // SolveMyHomeWorkAboutDLUP();
---
+++ utils.cpp
@@ -0,0 +1,129 @@
+template<typename T>
+Matrix<T> mult(Matrix<T> A, Matrix<T> B) {
+  if (A.empty() || B.empty() || A[0].size() != B.size()) throw logic_error("wrong sizes");
+  size_t N = A.size(), M = A[0].size(), K = B[0].size();
+  Matrix<T> R(N, vector<T>(K, T(0)));
+  for (size_t i = 0; i < N; i++) {
+    for (size_t j = 0; j < K; j++) {
+      for (size_t k = 0; k < M; k++) {
+        R[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
+  return R;
+}
+
+template<typename T>
+Matrix<T> generateRandomSymmetricMatrix(int size_, int min_elem = -100, int max_elem = 100) {
+  std::uniform_int_distribution<std::mt19937_64::result_type> udist(min_elem, max_elem);
+  std::mt19937_64 generator(std::random_device{}());
+  Matrix<T> a(size_, vector<T>(size_));
+  for (int i = 0; i < size_; ++i) {
+    for (int j = 0; j < size_; ++j) {
+      if (j < i) {
+        a[i][j] = a[j][i];
+      } else {
+        a[i][j] = udist(generator);
+      }
+    }
+  }
+  return a;
+}
+
+double EPS = 1e-5;
+
+inline bool compareDouble(double d1, double d2) {
+  return abs(d1 - d2) <= EPS;
+}
+
+inline bool compareVectorOfDouble(const vector<double>& A1, const vector<double>& A2) {
+  assert(A1.size() == A2.size());
+  for (int i = 0; i < A1.size(); ++i) {
+    if (!compareDouble(A1[i], A2[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+inline bool compareMatrixOfDouble(const Matrix<double>& A1, const Matrix<double>& A2) {
+  assert(A1.size() == A2.size());
+  for (int i = 0; i < A1.size(); ++i) {
+    if (!compareVectorOfDouble(A1[i], A2[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+template<typename T>
+vector<T> generateAnsMatrix(const Matrix<T>& A, const vector<T>& x) {
+  vector<T> ans(A.size());
+  for (int i = 0; i < A.size(); ++i) {
+    ans[i] = T(0);
+    for (int j = 0; j < A.size(); ++j) {
+      ans[i] += x[j] * A[i][j];
+    }
+  }
+  return ans;
+}
+
+
+
+/// Nx1 == true  <=> ans.size() == n && ans[i].size() == 1 \forall i
+/// Nx1 == false <=> ans.size() == 1 && ans[i].size() == n \forall i
+template<typename T>
+Matrix<T> make2Dfrom1D(const vector<T>& a, bool Nx1 = true) {
+  if (Nx1) {
+    Matrix<T> ans(a.size(), vector<T>(1));
+    for (int i = 0; i < a.size(); ++i) {
+      ans[i][0] = a[i];
+    }
+    return ans;
+  } else {
+    Matrix<T> ans(1, vector<T>(a.size()));
+    for (int i = 0; i < a.size(); ++i) {
+      ans[0][i] = a[i];
+    }
+    return ans;
+  }
+}
+
+template<typename T>
+vector<T> make1Dfrom2D(const Matrix<T>& a) {
+  if (a.size() != 1) {
+    vector<T> ans(a.size());
+    for (int i = 0; i < a.size(); ++i) {
+      ans[i] = a[i][0];
+    }
+    return ans;
+  } else {
+    vector<T> ans(a[0].size());
+    for (int i = 0; i < a[0].size(); ++i) {
+      ans[i] = a[0][i];
+    }
+    return ans;
+  }
+}
+
+template<class T>
+struct greater_using_abs {
+  bool operator()(const T& x, const T& y) const { return abs(x) > abs(y); }
+};
+
+template<typename T>
+Matrix<T> getHWMatrix(int n) {
+  return {{n + 1, n / 2, -n / 2, 1},
+          {-n - 1, -(n + 1) / 2, (n + 1) / 2, -(n + 2) / 3},
+          {-n + 1, (n + 1) / 2, -(n + 2) / 3, n - 1},
+          {n / 3, -1, n, -n}};
+}
+
+template<typename T>
+Matrix<T> getIdentityMatrix(int n) {
+  Matrix<T> A(n, vector<T>(n, T(0)));
+  for (size_t i = 0; i < n; ++i) {
+    A[i][i] = T(1);
+  }
+  return A;
+}
